bc007c73cd379838ab51023f53dc5a44217dc062
Salvatore Trimarchi <path72@gmail.com>
2021-04-19 17:45:07 +0200

    due dubbi nel file di configurazione di Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,8 @@ let mix = require('laravel-mix');
 // mix.sass('src/app.scss', 'dist/');
 mix.sass('src/master.scss', 'css/');
 /**
+ * 
+ * 1.
  * Laravel Mix non scrive fuori dalla cartella di progetto (my-app/webpack.mix.js)? 
  * 
  * da dentro /my-app/ non funziona 
@@ -10,5 +12,11 @@ mix.sass('src/master.scss', 'css/');
  *		mix.sass('src/master.scss', '../../css/');
  * 
  * vuol dire che la cartella my-app deve coincidere con html-sass-first?
+ * 
+ * 2. 
+ * .gitignore > node_module
+ * legge solo le cartelle del suo livello, cercando fra queste node_module?
+ * oppure cerca node_module ovunque, anche nelle sottocartelle di progetto (dove esso stesso vive)?
+ * 
  * 
  */
